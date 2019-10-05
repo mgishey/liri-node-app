4186c1d384470523b54ab8a2e51f029d862a24e3
Mike Gishey <mgishey56@gmail.com>
2019-10-04 18:09:12 -0700

    spotify objects fetched

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-// Set up Enviroment
+// Set up Enviroment for spotify
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
@@ -12,8 +12,15 @@ spotify.search({ type: 'track', query: 'i want it that way', limit: 1}, function
     return console.log('Error occurred: ' + err);
   }
  
+  // variable for root of search
+var rs = data.tracks.items[0];
+//console.log(rs);
 
-console.log(data.tracks.items);
+console.log(rs.album.name);
+console.log(rs.artists[0].name);
+console.log(rs.name);
+console.log(rs.preview_url);
+
 
 });
 
