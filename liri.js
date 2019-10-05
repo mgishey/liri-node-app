d2c8fffb5c16da3a9537bf88d38fdd6eb2d0c2ea
Mike Gishey <mgishey56@gmail.com>
2019-10-04 17:03:46 -0700

    connected to spotify

--- liri.js
+++ liri.js
@@ -1,8 +1,21 @@
 // Set up Enviroment
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = newSpotify(keys.spotify);
+var Spotify = require('node-spotify-api');
 
-var nodeArgs = process.argv;
+var spotify = new Spotify(keys.spotify);
+//let spotId = spotify.credentials.id;
+//let spotSecret = spotify.credentials.secret;
+ 
+spotify.search({ type: 'track', query: 'i want it that way', limit: 1}, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
 
-console.log("nodeArgs is: " + nodeArgs);
+console.log(data.tracks.items);
+
+});
+
+
+console.log("Hello??");
