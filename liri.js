5512e2ebfe13186e2231bb308d0e271b2acda2e1
Mike Gishey <mgishey56@gmail.com>
2019-10-06 11:16:50 -0700

    movie-this working

--- liri.js
+++ liri.js
@@ -12,32 +12,33 @@ switch (command) {
     spotifySong();
     break;
   case "concert-this":
+    var concert = process.argv[3];
     concertThis();
     break;
   case "movie-this":
+    var movie = process.argv[3];
     movieThis();
     break;
   case "do-what-it-says":
     doWhatItSays();
     break;
   default: console.log("\n" + "liri.js" + " <command>" + "\n" +
-    "<spotify-this-song>" + "\n" +
-    "<concert-this>" + "\n" +
-    "<movie-this>" + "\n" +
+    "<spotify-this-song>" + " song" + "\n" +
+    "<concert-this>" + " concert" + "\n" +
+    "<movie-this>" + " movie" + "\n" +
     "<do-what-it-says>" + "\n\n");
 }
 
 function spotifySong() {
-  var songName = process.argv[3];
-  if (!songName) {
-    songName = "The Sign";
+
+  if (!option) {
+    option = "The Sign";
     artist = "Ace of Base";
   }
-  spotify.search({ type: 'track', query: songName, limit: 1 }, function (err, data) {
+  spotify.search({ type: 'track', query: option, limit: 1 }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-
     // variable for root of search
     var rs = data.tracks.items[0];
     //console.log(rs);
@@ -50,46 +51,26 @@ function spotifySong() {
 }
 
 function movieThis() {
-  var movie = process.argv[3];
-  axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
+  if (!option) {
+    option = "Mr. Nobody";
+  }
+  axios.get("http://www.omdbapi.com/?t=" + option + "&y=&plot=short&apikey=trilogy").then(
     function (response) {
-      //console.log(response);
+      // assign data root
       var mr = response.data;
-      console.log(mr);
-      /*
-      * Title of the movie.
-         * Year the movie came out.
-         * IMDB Rating of the movie.
-         * Rotten Tomatoes Rating of the movie.
-         * Country where the movie was produced.
-         * Language of the movie.
-         * Plot of the movie.
-         * Actors in the movie. */
-        
+      //  log out relevant data
       console.log("Title: " + mr.Title);
       console.log("Rating: " + mr.imdbRating);
       console.log("Rotten Tomatoes Rating: " + mr.Ratings[1].Value);
       console.log("Country Where Produced: " + mr.Country);
+      console.log("Language: " + mr.Language);
+      console.log("Plot: " + mr.Plot);
+      console.log("Actors: " + mr.Actors + "\n\n\n");
     })
     .catch(function (error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log("---------------Data---------------");
-        console.log(error.response.data);
-        console.log("---------------Status---------------");
-        console.log(error.response.status);
-        console.log("---------------Status---------------");
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an object that comes back with details pertaining to the error that occurred.
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
-      }
-      console.log(error.config);
+      console.log(error);
     });
 }
+
+
 console.log("Hello??");
